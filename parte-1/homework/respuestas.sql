37131ecb1d6eb46bd85ff609d0475000c160ae22
MarCaroP <39929791+MarCaroP@users.noreply.github.com>
2023-03-26 18:25:24 -0300

    Se completa la primera parte de los ejercicios

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -1,24 +1,57 @@
 /*
+EJERCICIOS CON SQL SERVER
 Clase 1:
 Respuestas:
 */
 
--- 1 Mostrar todos los productos dentro de la categoria electro junto con todos los detalles.
-select * from stg.product_master
-where categoria='Electro'
--- 2 Cuales son los producto producidos en China?
-select * from p
+use [SQL de cero a Messi];
 
+-- 1 Mostrar todos los productos dentro de la categoria electro junto con todos los detalles.
+select * from product_master
+where categoria = 'electro';
+-- 2 Cuales son los producto producidos en China?
+select * from product_master
+where origen like 'china'
 -- 3 Mostrar todos los productos de Electro ordenados por nombre.
+select * from product_master
+where categoria like 'Electro'
+order by nombre;
 -- 4 Cuales son las TV que se encuentran activas para la venta?
+select * from product_master
+where subcategoria like 'tv' and is_active =1
 -- 5 Mostrar todas las tiendas de Argentina ordenadas por fecha de apertura de las mas antigua a la mas nueva.
+select * from store_master sm
+where sm.pais like 'Argentina' and fecha_apertura is not null
+order by fecha_apertura;
 -- 6 Cuales fueron las ultimas 5 ordenes de ventas?
+select  top 5 * from order_line_sale
+order by fecha desc
 -- 7 Mostrar los primeros 10 registros de el conteo de trafico por Super store ordenados por fecha.
+select top 10 * from super_store_count
+order by fecha;
 -- 8 Cuales son los producto de electro que no son Soporte de TV ni control remoto.
+select * from product_master
+where subsubcategoria not like 'Soporte%' 
+AND subsubcategoria not like'Control%';
+
+select * from product_master
+where subsubcategoria not in ('Soporte','Control remoto');
+
 -- 9 Mostrar todas las lineas de venta donde el monto sea mayor a $100.000 solo para transacciones en pesos.
+select * from order_line_sale ol
+where ol.venta>100000 and ol.moneda='ARS'
 -- 10 Mostrar todas las lineas de ventas de Octubre 2022.
+select * from order_line_sale ol
+where year(ol.fecha)=2022 and MONTH(ol.fecha)=10
+
+select * from order_line_sale ol
+where ol.fecha between '2022-10-01' and '2022-10-30'
 -- 11 Mostrar todos los productos que tengan EAN.
+select * from product_master pm
+where pm.ean is not null
 -- 12 Mostrar todas las lineas de venta que que hayan sido vendidas entre 1 de Octubre de 2022 y 10 de Noviembre de 2022.
+select * from order_line_sale ol
+where ol.fecha between '2022-10-01' and '2022-11-10'
 
 
 
